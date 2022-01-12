7b0fa6caaa53e1471710307760ff6b7c7eb868da
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-12 12:46:13 -0500

    create some new unittests to test home view: status_code and resolve

--- Boards/boards/tests.py
+++ Boards/boards/tests.py
@@ -1,3 +1,17 @@
+# django
 from django.test import TestCase
+from django.urls import resolve, reverse
+# internal
+from . import views
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    """Home View Tests"""
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_resolve_url(self):
+        view = resolve('/')
+        self.assertEqual(view.func, views.home)
