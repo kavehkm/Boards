3f2f4b08a9ae32758473f1cd8db42a06d0f195e3
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-13 12:45:20 -0500

    add route into boards.urls: board_topics

--- Boards/boards/urls.py
+++ Boards/boards/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('boards/<int:pk>', views.board_topics, name='board_topics')
 ]
