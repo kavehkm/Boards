51e45b578515c332ee759dad816fd8602bc8322b
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-13 12:47:22 -0500

    add new method absolute_url into Board model to get link into board topics

--- Boards/boards/models.py
+++ Boards/boards/models.py
@@ -1,12 +1,16 @@
 # django
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Board(models.Model):
     """Board Model"""
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
+
+    def absolute_url(self):
+        return reverse('board_topics', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
