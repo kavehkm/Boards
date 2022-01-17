3ac1fea8511e6b7cd12c07ff2c750515f503e9c1
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-17 16:07:35 -0500

    add new routes with built-n CBV views for sign-in and sign-out and also define consts: LOGIN_REDIRECT_URL & LOGOUT_REDIRECT_URL in main settings

--- Boards/accounts/urls.py
+++ Boards/accounts/urls.py
@@ -7,4 +7,6 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
+    path('signin', auth_views.LoginView.as_view(), name='signin'),
+    path('signout', auth_views.LogoutView.as_view(), name='signout')
 ]
