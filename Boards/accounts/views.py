c0ca43674201a55b40010ae7ef411bfa1a51f1c2
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-17 15:46:08 -0500

    update accounts/view.py: 1) add signup view to handle users signup process

--- Boards/accounts/views.py
+++ Boards/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# django
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+# internal
+from .forms import SignupForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
