715abdfc2cdc8b24b08a28177e03b7afa79ca054
kavehkm <mehrbaniankaveh@gmail.com>
2022-01-21 02:10:57 -0500

    update board_topics view to return number of replies for each topic and also update related template

--- Boards/boards/views.py
+++ Boards/boards/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 # internal
+from django.db.models import Count
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 
@@ -13,7 +14,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
 @login_required
